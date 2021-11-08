6d2819b8699495b2c6ce75c17cdfc2218b561902
Alena Bulanova <kxzws54@gmail.com>
2021-11-08 22:06:36 +0300

    feat: add 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,39 @@
+const { stderr } = process;
+const fs = require("fs");
+const path = require("path");
 
+const folderPath = path.join(__dirname, 'files');
+const newFolderPath = path.join(__dirname, 'files-copy');
+
+fs.mkdir(newFolderPath, { recursive: true}, error => {
+  if (error) stderr.write(`Error fs.mkdir() code: ${error}`);
+});
+
+fs.readdir(newFolderPath, { withFileTypes: true }, (error, files) => {
+  if (error) {
+    stderr.write(`Error fs.readdir() code: ${error}`);
+  } else {
+    files.forEach((file) => {
+        let newFilePath = path.join(newFolderPath, file.name);
+        
+        fs.unlink(newFilePath, error => {
+          if (error) stderr.write(`Error fs.unlink() code: ${error}`);
+        })
+    });
+  }
+})
+
+fs.readdir(folderPath, { withFileTypes: true }, (error, files) => {
+  if (error) {
+    stderr.write(`Error fs.readdir() code: ${error}`);
+  } else {
+    files.forEach((file) => {
+        let filePath = path.join(folderPath, file.name);
+        let newFilePath = path.join(newFolderPath, file.name);
+        
+        fs.copyFile(filePath, newFilePath, error => {
+          if (error) stderr.write(`Error fs.copyFile() code: ${error}`);
+        })
+    });
+  }
+});
